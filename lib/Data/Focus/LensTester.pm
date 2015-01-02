600592e567b71ee7a3a20ade20014826e8c0456d
Toshio Ito <debug.ito@gmail.com>
2015-01-02 10:24:39 +0900

    LensTester: write SYNOPSIS.

--- lib/Data/Focus/LensTester.pm
+++ lib/Data/Focus/LensTester.pm
@@ -132,7 +132,28 @@ Data::Focus::LensTester - tester for Lens implementations
 
 =head1 SYNOPSIS
 
-TODO: write this.
+    use Test::More;
+    use Data::Focus::LensTester;
+    use Data::Focus::Lens::HashArray::Index;
+    
+    my $tester = Data::Focus::LensTester->new(
+        test_whole => sub { is_deeply($_[0], $_[1]) },
+        test_part  => sub { is($_[0], $_[1]) },
+        parts => [undef, 1, "str"]
+    );
+    
+    my $create_target = sub {
+        +{ foo => "bar" }
+    };
+    
+    my $lens = Data::Focus::Lens::HashArray::Index->new(
+        key => "foo"
+    );
+    
+    $tester->test_lens_laws(
+        lens => $lens, target => $create_target,
+        exp_focal_points => 1, exp_mutate => 1
+    );
 
 =head1 DESCRIPTION
 
