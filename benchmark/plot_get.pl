a5ea05b108b8d4291811427f048a0b1c236a3831
Toshio Ito <debug.ito@gmail.com>
2015-01-18 17:10:45 +0900

    benchmark/plot_get.pl: add POD.

--- benchmark/plot_get.pl
+++ benchmark/plot_get.pl
@@ -2,8 +2,16 @@
 use v5.10.0;
 use strict;
 use warnings;
+use Pod::Usage;
+use Getopt::Long qw(:config no_ignore_case bundling);
 use Gnuplot::Builder qw(gscript gdata);
 use JSON qw(decode_json);
+
+sub usage { pod2usage(-verbose => 2, -noperldoc => 1) }
+
+GetOptions(
+    "h|help" => \&usage
+);
 
 my $result_str = do { local $/; <> };
 my $result = decode_json($result_str);
@@ -24,3 +32,22 @@ my $script = gscript(
 );
 
 $script->plot(@datasets);
+
+__END__
+
+=pod
+
+=head1 NAME
+
+plot_get.pl - plot the result of get.pl
+
+=head1 SYNOPSIS
+
+    $ get.pl 1 10 20 > result.json
+    $ plot_get.pl result.json
+
+=head1 AUTHOR
+
+Toshio Ito
+
+=cut
