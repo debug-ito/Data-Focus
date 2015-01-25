24be3a44cadb7364dc8970c35f8023e7b27329fb
Toshio Ito <debug.ito@gmail.com>
2015-01-25 11:13:20 +0900

    benchmark/profile_get.pl: increase loop count to 1000

--- benchmark/profile_get.pl
+++ benchmark/profile_get.pl
@@ -4,5 +4,5 @@ use Data::Focus qw(focus);
 
 my $target = [{foo => "bar"}];
 my @lenses = map { Data::Focus->coerce_to_lens($_) } 0, "foo";
-my $val = focus($target)->get(@lenses) for 1..100;
+my $val = focus($target)->get(@lenses) for 1..1000;
 
